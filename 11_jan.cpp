1160e17af4cad92bb85a6b81d31d53413836bc8c
Harshit Gupta <harshitgupta0910@gmail.com>
2025-01-11 12:43:10 +0530

    day 11

---
+++ 11_jan.cpp
@@ -0,0 +1,50 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+class Solution {
+  public:
+    int longestUniqueSubstr(string &s) {
+        set<char> charSet;
+        int ml = 0;int ws = 0;
+        for (int we = 0; we < s.length(); we++) {
+            while (charSet.find(s[we]) != charSet.end()) {
+                charSet.erase(s[ws]);
+                ws++;
+            }
+            charSet.insert(s[we]);
+            ml = max(ml, we - ws + 1);
+        }
+
+        return ml;
+    }
+};
+
+return 0;
+}
