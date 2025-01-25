eed5c5428ede9b7e92c243844878e03f335c4e1e
Harshit Gupta <harshitgupta0910@gmail.com>
2025-01-25 10:06:49 +0530

    day 25

---
+++ 25_jan.cpp
@@ -0,0 +1,70 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+class Solution {
+  public:
+    Node* findFirstNode(Node* head) {
+         if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
+
+        Node *tortoise = head;
+        Node *hare = head;
+        while (hare->next != nullptr && hare->next->next != nullptr) {
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+
+            if (tortoise == hare) {
+                break;
+            }
+        }
+        if (hare->next == nullptr || hare->next->next == nullptr) {
+            return nullptr;
+        }
+        tortoise = head;
+        while (tortoise != hare) {
+            tortoise = tortoise->next;
+            hare = hare->next;
+        }
+        return tortoise;
+
+    }
+};
+return 0;
+}
