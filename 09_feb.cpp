690b156fcf320554744768fbc8dff2144cf1c131
Harshit Gupta <harshitgupta0910@gmail.com>
2025-02-09 00:12:49 +0530

    day 09

---
+++ 09_feb.cpp
@@ -0,0 +1,63 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+class Solution {
+  public:
+    int findMaxSum(Node *root) {
+        int maxSum = INT_MIN;
+        helper(root, maxSum);
+        return maxSum;
+    }
+    
+  private:
+    int helper(Node* node, int &maxSum) {
+        if (!node) return 0;
+        int leftMax = max(0, helper(node->left, maxSum));
+        int rightMax = max(0, helper(node->right, maxSum));
+        
+        maxSum = max(maxSum, leftMax + rightMax + node->data);
+        
+        return node->data + max(leftMax, rightMax);
+    }
+};
+return 0;
+}
