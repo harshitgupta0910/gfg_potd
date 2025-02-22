b14f9c64fd2a5cb9abdac6b6501da4c862905a89
Harshit Gupta <harshitgupta0910@gmail.com>
2025-02-23 01:51:06 +0530

    any

---
+++ 23_feb.cpp
@@ -0,0 +1,49 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+vector<int> nextLargerElement(vector<int>& arr) {
+    // code here
+int n = arr.size();
+vector<int> v(n, -1);
+stack<int> st;
+for(int i = 0; i < n; i++) {
+    while(!st.empty() and arr[i] > arr[st.top()]) {
+        v[st.top()] = arr[i];
+        st.pop();
+    }
+    st.push(i);
+}
+
+return v;
+    
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+return 0;
+}
