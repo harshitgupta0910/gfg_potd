e0082b3f7739d48eeea4b4e9be55fab071a34d6a
Harshit Gupta <harshitgupta0910@gmail.com>
2025-03-06 13:38:51 +0530

    any

---
+++ 06_mar.cpp
@@ -0,0 +1,49 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+int lcs(string &s1, string &s2) {
+    int m = s1.size();
+    int n = s2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+return 0;
+}
