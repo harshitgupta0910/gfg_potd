340346f71bd9677cdcabfacc1ddfb9f35f91cdc5
Harshit Gupta <harshitgupta0910@gmail.com>
2025-03-10 12:19:22 +0530

    any

---
+++ 10_mar.cpp
@@ -0,0 +1,52 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+int editDistance(string& s1, string& s2) {
+    int m = s1.length(), n = s2.length();
+vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+for (int i = 0; i <= m; i++) {
+    for (int j = 0; j <= n; j++) {
+        if (i == 0) {
+            dp[i][j] = j;
+        } else if (j == 0) {
+            dp[i][j] = i;
+        } else if (s1[i - 1] == s2[j - 1]) {
+            dp[i][j] = dp[i - 1][j - 1];
+        } else {
+            dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+        }
+    }
+}
+return dp[m][n];
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+return 0;
+}
