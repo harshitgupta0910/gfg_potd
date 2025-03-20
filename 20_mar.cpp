40bc4368ef724e8daa97e6c725a57e68efcd7ec6
Harshit Gupta <harshitgupta0910@gmail.com>
2025-03-20 18:22:06 +0530

    any

--- 20_mar.cpp
+++ 20_mar.cpp
@@ -24,18 +24,21 @@ typedef map<string, string> mpss;
 #define ll long long
 #define int ll
 using pii = pair<int, int>;
-int buy1 = INT_MIN, buy2 = INT_MIN;
-        int sell1 = 0, sell2 = 0;
-        
-        for (int price : prices) {
-            buy1 = max(buy1, -price);
-            sell1 = max(sell1, buy1 + price);
-            buy2 = max(buy2, sell1 - price);
-            sell2 = max(sell2, buy2 + price);
-        }
-        
-        return sell2;
+int maxProfit(vector<int> &prices) {
+    // code here
+    int buy1 = INT_MIN, buy2 = INT_MIN;
+    int sell1 = 0, sell2 = 0;
+    
+    for (int price : prices) {
+        buy1 = max(buy1, -price);
+        sell1 = max(sell1, buy1 + price);
+        buy2 = max(buy2, sell1 - price);
+        sell2 = max(sell2, buy2 + price);
+    }
+    
+    return sell2;
 
+}
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
