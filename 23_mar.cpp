9e6a78309c73c4e4c4fe5003ea97b43e5f5c5b96
Harshit Gupta <harshitgupta0910@gmail.com>
2025-03-23 14:38:34 +0530

    any

--- 23_mar.cpp
+++ 23_mar.cpp
@@ -28,7 +28,6 @@ int countWays(string &s) {int n = s.size();
     vector<int> dp(n + 1);
     dp[0] = 1;
     dp[1] = (s[0] != '0') ? 1 : 0;
-
     for (int i = 2; i <= n; i++) {
         if (s[i - 1] != '0') {
             dp[i] += dp[i - 1];
