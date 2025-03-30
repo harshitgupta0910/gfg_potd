48d85ab109ef04cd61c70bafcf48f5bcd532ae6f
Harshit Gupta <harshitgupta0910@gmail.com>
2025-03-30 11:20:48 +0530

    any

---
+++ 30_mar.cpp
@@ -0,0 +1,47 @@
+//Author :D HARSHIT 
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef set<int> sti;
+typedef set<long> stl;
+typedef set<char> stc;
+typedef set<string> str;
+typedef map<int, int> mpii;
+typedef map<int, char> mpic;
+typedef map<char, int> mpci;
+typedef map<int, string> mpis;
+typedef map<string, int> mpsi;
+typedef map<string, string> mpss;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define print(x) cout << x << '\n'
+#define ull unsigned long long
+#define ll long long
+#define int ll
+using pii = pair<int, int>;
+int startStation(vector<int> &gas, vector<int> &cost) {
+    // Your code here
+    int tg = 0,tc = 0,tank = 0,start = 0;
+    for (int i = 0; i < gas.size(); i++) {
+        tg += gas[i];
+        tc += cost[i];
+        tank += gas[i] - cost[i];
+        if (tank < 0) {
+            start = i + 1;
+            tank = 0;
+        }
+    }
+    return tg < tc ? -1 : start;
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+return 0;
+}
